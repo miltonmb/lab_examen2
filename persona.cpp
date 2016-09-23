ce9ae7084826c40fc3ca94c375c11c9f3b39b127
miltonmb <milton.pasos@gmail.com>
2016-09-23 13:31:01 -0600

    Primer commit

---
+++ persona.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string.h>
+
+persona::persona(string n,int e,string id){
+	nombre = n;
+	edad = e;
+	identidad = id;
+}
+
+persona::~persona(){
+
+}
---
+++ persona.h
@@ -0,0 +1,10 @@
+#include <iostream>
+class persona{
+	private:
+		string nombre;
+		int edad;
+		string identidad;
+	public:
+		persona(string,int,string);
+		~persona();
+}
